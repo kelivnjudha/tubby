5fbcfe3a236e162fd48c36a5ff7ed53b5a99dc4c
kelivnjudha <kelvin.7o2.k3@gmail.com>
2023-03-24 09:49:46 +0630

    Update GUI and Errors.

--- tubby_v0.2_demo/version_0.2_demo.py
+++ tubby_v0.2_demo/version_0.2_demo.py
@@ -4,6 +4,10 @@ import pytube # for yt video/ audio download
 import humanize # for display file size with MB/GB etc
 import customtkinter #for modern user interface
 import shutil #for file moving and copying
+import datetime
+import os
+import sys
+
 
 from tkinter import * #from tkinter import everything
 from tkinter import messagebox # from tkinter import messagebox for display errors
@@ -43,7 +47,7 @@ class App(customtkinter.CTk):
         videobutton = customtkinter.CTkButton(
             master=self,
             text="Download Video",
-            command=lambda: self.video_download(link.get()),
+            command=lambda: self.show_video_info(link.get(), "video"),
             height=35,
             corner_radius=8,
             )
@@ -52,13 +56,62 @@ class App(customtkinter.CTk):
         audiobutton = customtkinter.CTkButton(
             master=self,
             text="Download Audio",
-            command=lambda: self.audio_download(link.get()),
+            command=lambda: self.show_video_info(link.get(), "audio"),
             height=35,
             corner_radius=8,
         )
         audiobutton.place(relx=0.56,rely=0.17)
 
 ##################### Video Download ##################
+    def restart_app(self):
+        self.destroy()
+        os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+    def show_message_and_restart(self, title, message):
+        messagebox.showinfo(title, message)
+        self.restart_app()
+
+
+
+    def show_video_info(self, url, download_type):
+        try:
+            yt = YouTube(url)
+        except Exception as e:
+            messagebox.showerror("Error", str(e))
+            return
+
+        # Create a frame to hold the video information and buttons
+        info_frame = Frame(self, height=200, width=500)
+        info_frame.place(relx=0.5, rely=0.59, anchor=CENTER)
+
+        # Video title
+        title_label = Label(info_frame, text=f"Title: {yt.title}", wraplength=480)
+        title_label.pack(pady=2)
+
+        # Video length
+        length_label = Label(info_frame, text=f"Length: {str(datetime.timedelta(seconds=yt.length))}")
+        length_label.pack(pady=2)
+
+        # Video view count
+        views_label = Label(info_frame, text=f"Views: {yt.views}")
+        views_label.pack(pady=2)
+
+        # Video publish date
+        publish_label = Label(info_frame, text=f"Publish Date: {yt.publish_date}")
+        publish_label.pack(pady=2)
+
+        # Cancel and Continue buttons
+        cancel_button = Button(info_frame, text="Cancel", command=info_frame.destroy)
+        cancel_button.pack(side=LEFT, padx=10, pady=10)
+
+        if download_type == "video":
+            continue_button = Button(info_frame, text="Continue", command=lambda: [self.video_download(url), info_frame.destroy()])
+        elif download_type == "audio":
+            continue_button = Button(info_frame, text="Continue", command=lambda: [self.audio_download(url), info_frame.destroy()])
+
+        continue_button.pack(side=RIGHT, padx=10, pady=10)
+
 
     def video_download(self, url):
         index = -1
@@ -66,7 +119,7 @@ class App(customtkinter.CTk):
             yt=YouTube(url)
         except Exception as e:
             messagebox.showerror("Error",str(e))
-        video_stream = yt.streams.filter(only_video=False, only_audio=False, mime_type='video/mp4')
+        video_stream = yt.streams.filter(progressive=True, mime_type='video/mp4')
 
         if not video_stream:
             messagebox.showinfo("Tubby", "No video streams available for this URL")
@@ -106,13 +159,14 @@ class App(customtkinter.CTk):
         canvas.configure(scrollregion=canvas.bbox("all"))
 
         self.stream_list = stream_list
+
     def download_selected_stream(self, stream):
         download_path = askdirectory()
         if not download_path:
             return
         try:
             stream.download(output_path=download_path)
-            messagebox.showinfo("Tubby", "Video downloaded successfully")
+            self.show_message_and_restart("Tubby", "Video downloaded successfully")
         except Exception as e:
             messagebox.showerror("Error", f"An error occurred while downloading: {e}")
 
@@ -124,7 +178,7 @@ class App(customtkinter.CTk):
             yt=YouTube(url)
         except Exception as e:
             messagebox.showerror("Error",str(e))
-        video_stream = yt.streams.filter(only_audio=True, mime_type='audio/mp3')
+        video_stream = yt.streams.filter(only_audio=True, mime_type='audio/mp4')
 
         if not video_stream:
             messagebox.showinfo("Tubby", "No video streams available for this URL")
@@ -132,10 +186,10 @@ class App(customtkinter.CTk):
         
         # Create a frame to hold the stream options and scrollbar
         stream_frame = Frame(self, height=200, width=500)
-        stream_frame.place(relx=0.32, rely=0.5, anchor=CENTER)
+        stream_frame.place(relx=0.5, rely=0.59, anchor=CENTER)
 
         # Create a canvas to hold the frame and attach the scrollbar to the canvas
-        canvas = Canvas(stream_frame, height=200, width=170)
+        canvas = Canvas(stream_frame, height=300, width=348)
         scrollbar = Scrollbar(stream_frame, orient=VERTICAL, command=canvas.yview)
         canvas.configure(yscrollcommand=scrollbar.set)
         scrollbar.pack(side=RIGHT, fill=Y)
@@ -146,7 +200,7 @@ class App(customtkinter.CTk):
         canvas.create_window((0, 0), window=stream_list, anchor=NW)
 
         for i, stream in enumerate(video_stream):
-            button_text = f"{stream.resolution} @ {stream.fps}fps"
+            button_text = f"Audio - {stream.abr}"
 
             # Button for stream option
             stream_button = customtkinter.CTkButton(
@@ -155,6 +209,7 @@ class App(customtkinter.CTk):
                 command=lambda stream=stream: self.download_selected_stream(stream),
                 height=35,
                 corner_radius=8,
+                width=320
             )
             stream_button.pack(fill=X, padx=10, pady=5)
 
